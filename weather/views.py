d56989f22b298c76f85a2ef573de143b151160d3
gemma gonputh <gemmage3@hotmail.co.uk>
2020-10-20 17:03:38 +0100

    View Update

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,12 @@
-
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
+    url = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=b5fbc38b68ebe3b05eb0a099c64a668c'
+    city = 'London'
+
+    r = requests.get(url.format(city))
+    print(r.text)
     return render(request, 'weather/weather.html')
