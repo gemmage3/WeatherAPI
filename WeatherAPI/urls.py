7428fd165f12705aa5b535bfe01a1bb3757c7518
gemma gonputh <gemmage3@hotmail.co.uk>
2020-10-21 15:38:00 +0100

    Added CSS file

--- WeatherAPI/urls.py
+++ WeatherAPI/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from weather.views import index, delete_city
 
@@ -25,3 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
